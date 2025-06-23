cd4b3e04df9dcebc4b9dbdf4c2110fa765573565
muncho-dev-aniket <aniket@muncho.in>
2025-06-23 20:52:52 +0530

    feat: integrate React Query for data fetching in main application

--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,20 @@ import './index.css'
 import App from './App.jsx'
 import { ThemeProvider } from './contexts/ThemeContext.jsx'
 import { BrowserRouter } from "react-router-dom";
+import {
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query'
+
+const queryClient = new QueryClient()
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
       <ThemeProvider>
-        <App />
+        <QueryClientProvider client={queryClient}>
+          <App />
+        </QueryClientProvider>
       </ThemeProvider>
     </BrowserRouter>
   </StrictMode>,
